b770ae7dde0dcb8bbd2f7372701d48d2d027929e
IsaiStormBlesed <79279643+IsaiStormBlesed@users.noreply.github.com>
2021-05-27 13:38:36 -0500

    Add files via upload

---
+++ utilities.js
@@ -0,0 +1,11 @@
+const debounce = (func, delay) => {
+	let timeId
+	return (...args) => {
+		if(timeId) {
+			clearTimeout(timeId)
+		}
+		timeId = setTimeout(() => {
+			func.apply(null, args) 
+		}, delay)
+	}
+}
